21ec11b9b21fd2e3546b9b4833624f685cc246c4
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2019-12-07 09:56:48 -0600

    making things a bit more object oriented

--- day1/solution.py
+++ day1/solution.py
@@ -1,21 +1,21 @@
-def calculate_fuel(num):
-    return int(num / 3) - 2
+class Day1Solution():
+    def calculate_fuel(self, num):
+        return int(num / 3) - 2
 
-def calculate_fuel_recursive(num):
-    current_fuel_calc = calculate_fuel(num)
+    def calculate_fuel_recursive(self, num):
+        current_fuel_calc = self.calculate_fuel(num)
 
-    if current_fuel_calc <= 0:
-        return 0
+        if current_fuel_calc <= 0:
+            return 0
 
-    return current_fuel_calc + calculate_fuel_recursive(current_fuel_calc)
+        return current_fuel_calc + self.calculate_fuel_recursive(current_fuel_calc)
 
-def convert_to_int(str_num):
-    return int(str_num)
+    def calculate_fuel_requirements(self, map_func):
+        input_file = open("input.txt", "r")
+        file_content = map(lambda x: int(x), input_file.readlines())
+        return sum(map(map_func, file_content))
 
-def calculate_fuel_requirements(map_func):
-    input_file = open("input.txt", "r")
-    file_content = map(convert_to_int, input_file.readlines())
-    return sum(map(map_func, file_content))
+solution = Day1Solution()
 
-print(calculate_fuel_requirements(calculate_fuel))
-print(calculate_fuel_requirements(calculate_fuel_recursive))
+print(solution.calculate_fuel_requirements(solution.calculate_fuel))
+print(solution.calculate_fuel_requirements(solution.calculate_fuel_recursive))
