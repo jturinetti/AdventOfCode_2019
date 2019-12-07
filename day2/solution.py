d4a26147b94974346517b5d24d8f449ceb481529
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2019-12-07 10:20:55 -0600

    set up files and input for day 2 problems

---
+++ day2/solution.py
@@ -0,0 +1,11 @@
+class Day2Solution():
+    def run_intcode_impl(self, int_arr):
+        # TODO
+        return 0
+
+    def run_intcode(self):
+        # TODO
+        return 0
+
+solution = Day2Solution()
+print(solution.run_intcode())
