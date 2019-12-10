069ed93826f24924625b4d908db8423721d6c97c
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-12-09 22:24:26 -0600

    move .gitignore file; create F# files for Day 2 solution; basic F# funcs

---
+++ day2/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+module Day2Solution =
+    // this solution expects all input to be on a single line
+    let readFileLine (filePath:string) = Seq.toList(File.ReadLines(filePath)).[0]
+
+    let parseStringToIntArray (str:string) = str.Split ',' |> Array.map (fun x -> x |> int)
+
+[<EntryPoint>]
+// TESTING: prints contents of parsed array
+let main argv =        
+    for i in Day2Solution.parseStringToIntArray (Day2Solution.readFileLine argv.[0]) do printfn "%s" (i.ToString())    
+    0 // return an integer exit code
