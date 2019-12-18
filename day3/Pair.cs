37d5a22d0b3e27fde274e41bc80429abca8699cb
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2019-12-17 18:19:45 -0600

    WIP

--- day3/Pair.cs
+++ day3/Pair.cs
@@ -1,5 +1,11 @@
 public class Pair
 {
-    public int X {get;set;}
-    public int Y {get;set;}
+    public Pair(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X {get;}
+    public int Y {get;}
 }
