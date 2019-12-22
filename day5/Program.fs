e9e3b0a4a70bc2f1f72788a4ffa662ddab399dc6
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-12-22 10:40:07 -0600

    major cleanup and refactoring, now i can be proud of this

--- day5/Program.fs
+++ day5/Program.fs
@@ -9,39 +9,41 @@ module Day5Solution =
 
     let parseStringToIntArray (str:string) = str.Split ',' |> Array.map (fun x -> x |> int) |> Seq.toList
 
-    let retrieveParameter(instructions: int list, currentInstruction: int, instructionOffset: int, parameterMode: int) = 
+    let getValueByParameterMode(instructions: int list, currentInstruction: int, instructionOffset: int, parameterMode: int) = 
         match parameterMode with 
         | 0 -> instructions.[instructions.[currentInstruction + instructionOffset]]
         | 1 -> instructions.[currentInstruction + instructionOffset]
         | _ -> raise(InvalidParameterModeException(parameterMode))
 
+    // not especially proud of this function but it works
     let rec parseParameterModes(opCodeParameters: string, parameterModeArray: int list) = 
-        // TODO: this could be a bit smarter
-        if System.String.IsNullOrEmpty(opCodeParameters)
+        if parameterModeArray.Length = 2
         then
-            if parameterModeArray.Length = 1
-            then
-                List.concat [parameterModeArray; [0]]
-            else
-                parameterModeArray
+            parameterModeArray
+        elif System.String.IsNullOrEmpty(opCodeParameters)
+        then
+            parseParameterModes(opCodeParameters, List.concat [parameterModeArray; [0]])
         else
             let nextParameterMode = opCodeParameters.[opCodeParameters.Length - 1].ToString() |> int
             parseParameterModes(opCodeParameters.Substring(0, opCodeParameters.Length - 1), List.concat [parameterModeArray; [nextParameterMode]])
 
-    let performAddition (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
-        retrieveParameter(instructions, currentInstruction, 1, parameter1Mode) + retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
+    let add (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
+        getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) + getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
 
-    let performMultiplication (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) =
-        retrieveParameter(instructions, currentInstruction, 1, parameter1Mode) * retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
+    let multiply (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) =
+        getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) * getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
 
-    let performJumpIfTrueOperation (instructions: int list, currentInstruction: int, parameter1Mode: int) = 
-        retrieveParameter(instructions, currentInstruction, 1, parameter1Mode) <> 0
+    let jumpIfTrue (instructions: int list, currentInstruction: int, parameter1Mode: int) = 
+        getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) <> 0
 
-    let performLessThanOperation (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
-        retrieveParameter(instructions, currentInstruction, 1, parameter1Mode) < retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
+    let jumpIfFalse (instructions: int list, currentInstruction: int, parameter1Mode: int) = 
+        not(jumpIfTrue(instructions, currentInstruction, parameter1Mode))
 
-    let performEqualToOperation (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
-        retrieveParameter(instructions, currentInstruction, 1, parameter1Mode) = retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
+    let lessThan (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
+        getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) < getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
+
+    let equalTo (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
+        getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) = getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)    
 
     let rec intCodeProcessor (instructions: int list, currentInstruction: int, input: int) = 
         let originalOpCode = instructions.[currentInstruction]
@@ -58,74 +60,56 @@ module Day5Solution =
             then
                 parseParameterModes(stringOpCode.Substring(0, stringOpCode.Length - 2), List.Empty)
             else
-                [0; 0]
+                parseParameterModes("", List.Empty)
 
         let parameter1Mode = parameterModeList.[0]
         let parameter2Mode = parameterModeList.[1]
         
         // printfn "Current instruction set: %A" instructions
         // printfn "OpCode: %s" stringOpCode
-
-        if opCode = 99 
-        then 
-            instructions
-        elif opCode = 3
-        then
+        
+        match opCode with
+        | 99 -> instructions
+        | 1 -> mathFunc(add, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | 2 -> mathFunc(multiply, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | 3 -> 
             let targetIndex = instructions.[currentInstruction + 1]
             let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [input]; instructions.[targetIndex + 1..]]
             intCodeProcessor(updatedInstructions, currentInstruction + 2, input)
-        elif opCode = 4
-        then
-            let valueToPrint = retrieveParameter(instructions, currentInstruction, 1, parameter1Mode)
+        | 4 ->
+            let valueToPrint = getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode)
             printfn "%i" valueToPrint
             intCodeProcessor(instructions, currentInstruction + 2, input)
-        elif opCode = 5
-        then
-            if performJumpIfTrueOperation(instructions, currentInstruction, parameter1Mode)
+        | 5 -> jumpFunc(jumpIfTrue, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | 6 -> jumpFunc(jumpIfFalse, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | 7 -> comparisonFunc(lessThan, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | 8 -> comparisonFunc(equalTo, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+        | _ -> raise (InvalidIntCodeSequenceException(instructions.[currentInstruction], currentInstruction))
+
+    and mathFunc (func: (int list * int * int * int -> int), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
+        let calculatedValue = func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
+        let targetIndex = instructions.[currentInstruction + 3]
+        let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [calculatedValue]; instructions.[targetIndex + 1..]]
+        intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
+
+    and jumpFunc (func: (int list * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
+        if func(instructions, currentInstruction, parameter1Mode)
             then
-                let newInstructionPointer = retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
+                let newInstructionPointer = getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
                 intCodeProcessor(instructions, newInstructionPointer, input)
             else
                 intCodeProcessor(instructions, currentInstruction + 3, input)
-        elif opCode = 6
-        then
-            if not(performJumpIfTrueOperation(instructions, currentInstruction, parameter1Mode))
+
+    and comparisonFunc (func: (int list * int * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
+        let valueToWrite = 
+            if func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
             then
-                let newInstructionPointer = retrieveParameter(instructions, currentInstruction, 2, parameter2Mode)
-                intCodeProcessor(instructions, newInstructionPointer, input)
+                1
             else
-                intCodeProcessor(instructions, currentInstruction + 3, input)
-        elif opCode = 7
-        then
-            let valueToWrite = 
-                if performLessThanOperation(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-                then
-                    1
-                else
-                    0
-            let targetIndex = instructions.[currentInstruction + 3]
-            let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [valueToWrite]; instructions.[targetIndex + 1..]]
-            intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
-        elif opCode = 8
-        then
-            let valueToWrite = 
-                if performEqualToOperation(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-                then
-                    1
-                else
-                    0
-            let targetIndex = instructions.[currentInstruction + 3]
-            let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [valueToWrite]; instructions.[targetIndex + 1..]]
-            intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
-        else
-            let newValue = 
-                match opCode with 
-                | 1 -> performAddition(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-                | 2 -> performMultiplication(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-                | _ -> raise (InvalidIntCodeSequenceException(instructions.[currentInstruction], currentInstruction))
-            let targetIndex = instructions.[currentInstruction + 3]
-            let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [newValue]; instructions.[targetIndex + 1..]]
-            intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
+                0
+        let targetIndex = instructions.[currentInstruction + 3]
+        let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [valueToWrite]; instructions.[targetIndex + 1..]]
+        intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
 
 [<EntryPoint>]
 let main argv =
