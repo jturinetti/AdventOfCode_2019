3fee864a93d5180881292254b128b9dd1f93168d
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-12-29 14:58:42 -0600

    completing day 8 part 2

--- day8/Program.fs
+++ day8/Program.fs
@@ -2,9 +2,7 @@
 open System.IO
 
 module Day8Solution = 
-
-    let imageWidth = 25
-    let imageHeight = 6
+    exception InvalidLayeringException of obj
 
     let readFileLine (filePath:string) = Seq.toList(File.ReadLines(filePath)).[0]
 
@@ -21,7 +19,7 @@ module Day8Solution =
     let findDigitTuple(tupleList: (char * int) list, charToMatch: char) = 
         List.find (fun (x, y) -> x = charToMatch) tupleList
 
-    let solveProblem1(input:string) = 
+    let solveProblem1(input:string, imageWidth: int, imageHeight: int) = 
         let layers = splitLayers(input, imageWidth * imageHeight, 0, List.empty)
     
         let digitCounts = List.map(fun elem -> digitCount(elem)) layers
@@ -37,10 +35,36 @@ module Day8Solution =
 
         result
 
+    let rec resolvePixel(layerPixels: char list) = 
+        match layerPixels with
+        | [] -> raise(InvalidLayeringException())
+        | xs :: tail -> 
+            match xs with
+            | '2' -> resolvePixel tail
+            | _ -> xs.ToString()
+
+    let rec resolveLayers(layerLists: string list, currentPixelIndex: int, layerOutput: string list, layerRowLength: int) = 
+        match currentPixelIndex with
+        | index when index = layerRowLength -> layerOutput
+        | _ -> 
+            let nextPixel = List.map (fun (x:string) -> x.[currentPixelIndex]) layerLists
+            let resolvedPixel = resolvePixel nextPixel
+            resolveLayers(layerLists, currentPixelIndex + 1, layerOutput @ [resolvedPixel], layerRowLength)
+
+    let solveProblem2(input:string, imageWidth: int, imageHeight: int) = 
+        let layers = (splitLayers(input, imageWidth * imageHeight, 0, List.empty))
+        let updatedLayers = resolveLayers(layers, 0, List.empty, imageWidth * imageHeight)
+        splitLayers(updatedLayers |> String.concat "", imageWidth, 0, List.empty)
+        
 [<EntryPoint>]
 let main argv =
     let input = Day8Solution.readFileLine argv.[0]
+    let imageWidth = argv.[1] |> int
+    let imageHeight = argv.[2] |> int
     
-    printfn "%i" (Day8Solution.solveProblem1(input))
-
+    let result = Day8Solution.solveProblem2(input, imageWidth, imageHeight)
+    
+    for pixelRow in result do
+        printfn "%s" pixelRow
+        
     0 // return an integer exit code
