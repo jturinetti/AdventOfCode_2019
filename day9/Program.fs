1792007d4b03785573fde7180ed81a0d34fa96c8
Jeremy Turinetti <jeremy.turinetti@gmail.com>
2019-12-30 15:40:01 -0600

    cleanup before starting day9

--- day9/Program.fs
+++ day9/Program.fs
@@ -4,6 +4,8 @@ module Day9Solution =
     exception InvalidIntCodeSequenceException of int * int
     exception InvalidParameterModeException of int
 
+    let mutable debugMode = false
+
     // this solution expects all input to be on a single line
     let readFileLine (filePath:string) = Seq.toList(File.ReadLines(filePath)).[0]
 
@@ -45,7 +47,7 @@ module Day9Solution =
     let equalTo (instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int) = 
         getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode) = getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)    
 
-    let rec intCodeProcessor (instructions: int list, currentInstruction: int, input: int) = 
+    let rec intCodeProcessor (instructions: int list, currentInstruction: int, input: int, relativeBase: int) = 
         let originalOpCode = instructions.[currentInstruction]
         let stringOpCode = originalOpCode.ToString()
         let opCode = 
@@ -62,60 +64,65 @@ module Day9Solution =
             else
                 parseParameterModes("", List.Empty)
 
+        if debugMode
+        then
+            printfn "OpCode %i" originalOpCode
+            printfn "Parameter Modes %A" parameterModeList
+
         let parameter1Mode = parameterModeList.[0]
         let parameter2Mode = parameterModeList.[1]
         
-        // printfn "Current instruction set: %A" instructions
-        // printfn "OpCode: %s" stringOpCode
-        
         match opCode with
         | 99 -> instructions
-        | 1 -> mathFunc(add, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
-        | 2 -> mathFunc(multiply, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
-        | 3 -> 
-            let targetIndex = instructions.[currentInstruction + 1]
-            let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [input]; instructions.[targetIndex + 1..]]
-            intCodeProcessor(updatedInstructions, currentInstruction + 2, input)
+        | 1 -> mathFunc(add, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
+        | 2 -> mathFunc(multiply, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
+        | 3 ->
+            let updatedInstructions = 
+                let targetIndex = instructions.[currentInstruction + 1]
+                List.concat [instructions.[..targetIndex - 1]; [input]; instructions.[targetIndex + 1..]]
+            intCodeProcessor(updatedInstructions, currentInstruction + 2, input, relativeBase)
         | 4 ->
-            let valueToPrint = getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode)
-            printfn "%i" valueToPrint
-            intCodeProcessor(instructions, currentInstruction + 2, input)
-        | 5 -> jumpFunc(jumpIfTrue, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
-        | 6 -> jumpFunc(jumpIfFalse, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
-        | 7 -> comparisonFunc(lessThan, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
-        | 8 -> comparisonFunc(equalTo, instructions, currentInstruction, parameter1Mode, parameter2Mode, input)
+            printfn "%i" (getValueByParameterMode(instructions, currentInstruction, 1, parameter1Mode))
+            intCodeProcessor(instructions, currentInstruction + 2, input, relativeBase)
+        | 5 -> jumpFunc(jumpIfTrue, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
+        | 6 -> jumpFunc(jumpIfFalse, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
+        | 7 -> comparisonFunc(lessThan, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
+        | 8 -> comparisonFunc(equalTo, instructions, currentInstruction, parameter1Mode, parameter2Mode, input, relativeBase)
         | _ -> raise (InvalidIntCodeSequenceException(instructions.[currentInstruction], currentInstruction))
 
-    and mathFunc (func: (int list * int * int * int -> int), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
-        let calculatedValue = func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-        let targetIndex = instructions.[currentInstruction + 3]
-        let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [calculatedValue]; instructions.[targetIndex + 1..]]
-        intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
+    and mathFunc (func: (int list * int * int * int -> int), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int, relativeBase: int) = 
+        let updatedInstructions = 
+            let calculatedValue = func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
+            let targetIndex = instructions.[currentInstruction + 3]
+            List.concat [instructions.[..targetIndex - 1]; [calculatedValue]; instructions.[targetIndex + 1..]]
+        intCodeProcessor(updatedInstructions, currentInstruction + 4, input, relativeBase)
 
-    and jumpFunc (func: (int list * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
-        if func(instructions, currentInstruction, parameter1Mode)
-            then
-                let newInstructionPointer = getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
-                intCodeProcessor(instructions, newInstructionPointer, input)
-            else
-                intCodeProcessor(instructions, currentInstruction + 3, input)
+    and jumpFunc (func: (int list * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int, relativeBase: int) = 
+        let funcResult = func(instructions, currentInstruction, parameter1Mode)
+        match funcResult with
+        | true -> 
+            let newInstructionPointer = getValueByParameterMode(instructions, currentInstruction, 2, parameter2Mode)
+            intCodeProcessor(instructions, newInstructionPointer, input, relativeBase)
+        | false -> intCodeProcessor(instructions, currentInstruction + 3, input, relativeBase)
 
-    and comparisonFunc (func: (int list * int * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int) = 
+    and comparisonFunc (func: (int list * int * int * int -> bool), instructions: int list, currentInstruction: int, parameter1Mode: int, parameter2Mode: int, input: int, relativeBase: int) = 
         let valueToWrite = 
-            if func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
-            then
-                1
-            else
-                0
-        let targetIndex = instructions.[currentInstruction + 3]
-        let updatedInstructions = List.concat [instructions.[..targetIndex - 1]; [valueToWrite]; instructions.[targetIndex + 1..]]
-        intCodeProcessor(updatedInstructions, currentInstruction + 4, input)
+            let funcResult = func(instructions, currentInstruction, parameter1Mode, parameter2Mode)
+            match funcResult with
+            | true -> 1
+            | false -> 0
+        
+        let updatedInstructions = 
+            let targetIndex = instructions.[currentInstruction + 3]
+            List.concat [instructions.[..targetIndex - 1]; [valueToWrite]; instructions.[targetIndex + 1..]]
+        intCodeProcessor(updatedInstructions, currentInstruction + 4, input, relativeBase)
 
 [<EntryPoint>]
 let main argv =
     let inputFileName = argv.[0]
     let input = argv.[1] |> int
+    Day9Solution.debugMode <- argv.Length > 2 && argv.[2] = "debug"
     let instructions = Day9Solution.parseStringToIntArray (Day9Solution.readFileLine inputFileName)
-    let resultArray = Day9Solution.intCodeProcessor (instructions, 0, input)
-    // printfn "Final instruction set: %A" resultArray
+    Day9Solution.intCodeProcessor (instructions, 0, input, 0) |> ignore
+    
     0 // return an integer exit code
